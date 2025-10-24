ebd37a548c5ee9660b0d9ce3368b8a7ba65fc6aa
Kairm Noorani <nitrileglovesnow@gmail.com>
2025-10-24 03:32:12 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -7,8 +7,8 @@ function getValue<T>(key: string, defaultValue: T): T {
     if (typeof window === 'undefined') {
         return defaultValue;
     }
-    const storedValue = window.localStorage.getItem(key);
     try {
+        const storedValue = window.localStorage.getItem(key);
         return storedValue ? JSON.parse(storedValue) : defaultValue;
     } catch (error) {
         console.error(`Error parsing localStorage key “${key}”:`, error);
@@ -17,10 +17,17 @@ function getValue<T>(key: string, defaultValue: T): T {
 }
 
 export function useLocalStorage<T>(key: string, defaultValue: T): [T, React.Dispatch<React.SetStateAction<T>>] {
-    const [value, setValue] = useState(() => getValue(key, defaultValue));
+    const [value, setValue] = useState(defaultValue);
 
     useEffect(() => {
-        window.localStorage.setItem(key, JSON.stringify(value));
+        setValue(getValue(key, defaultValue));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [key]);
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            window.localStorage.setItem(key, JSON.stringify(value));
+        }
     }, [key, value]);
 
     return [value, setValue];
