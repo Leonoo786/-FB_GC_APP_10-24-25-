15dd29a32c443e099fc20cd59577601561c42932
Kairm Noorani <nitrileglovesnow@gmail.com>
2025-11-03 22:07:51 +0000

    Try fixing this error: `Runtime Error: Error: Firebase: Error (auth/inva

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,3 @@
-
 // Follow this pattern to import other Firebase services
 // import { } from 'firebase/<service>';
 
@@ -6,20 +5,22 @@
 // import * as admin from 'firebase-admin';
 
 const firebaseConfig = {
-  "projectId": "studio-8302502127-557bf",
-  "appId": "1:259840996333:web:7de21ed4a3798cea26ade4",
-  "apiKey": "AIzaSyA3VW-0Eb5eD1zl5iTV7JA-Lbf6NaDdNjY",
-  "authDomain": "studio-8302502127-557bf.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "259840996333"
+  projectId: 'studio-8302502127-557bf',
+  appId: '1:259840996333:web:7de21ed4a3798cea26ade4',
+  apiKey: 'AIzaSyA3VW-0Eb5eD1zl5iTV7JA-Lbf6NaDdNjY',
+  authDomain: 'studio-8302502127-557bf.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '259840996333',
 };
 
 export function getFirebaseConfig() {
-    //
-    if (!firebaseConfig || !firebaseConfig.apiKey) {
-      throw new Error('No Firebase configuration object provided.' + '\n' +
-      'Add your web app\'s configuration object to firebase-config.ts');
-    } else {
-      return firebaseConfig;
-    }
+  if (!firebaseConfig || !firebaseConfig.apiKey) {
+    throw new Error(
+      'No Firebase configuration object provided.' +
+        '\n' +
+        "Add your web app's configuration object to firebase-config.ts"
+    );
+  } else {
+    return firebaseConfig;
+  }
 }
