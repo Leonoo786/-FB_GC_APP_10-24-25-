7852e9d0da36f79968e6ec2935b7dabfe633f7e1
Kairm Noorani <nitrileglovesnow@gmail.com>
2025-11-04 21:38:18 +0000

    Try fixing this error: `Runtime Error: Error: useFirebase must be used w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from '@/components/ui/toaster';
 import { ThemeProvider } from '@/components/theme-provider';
 
 export const metadata: Metadata = {
-  title: 'ConstructAI',
+  title: 'Company Name',
   description: 'Construction Project Management Application',
   manifest: '/manifest.json',
 };
